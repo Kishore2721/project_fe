0068b8ded0741a0a84529be929a28ce7b25babb9
aiswarya <ashzzaishwarya@gmail.com>
2025-06-11 15:49:33 +0530

    pulling from online repo to make code uniform

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import Registerpage from './components/RegisterPage';
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
 import UserCarDetails from './components/UserCarDetails';
 import UserStatus from './components/UserStatus';
@@ -12,34 +12,26 @@ import FixMyRideHomepage from './components/FixMyRideHomepage';
 import Forgotpass from './components/Forgotpass';
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
-
-
 function App() {
-
-  return(
+  return (
     <div>
+      <Router>
+        <Routes>
+          <Route path='/Registerpage' element={<Registerpage />}></Route>
+          <Route path='/LoginPage' element={<LoginPage />}></Route>
+          <Route path='/UserCarDetails' element={<UserCarDetails />}></Route>
+          <Route path='/UserStatus' element={<UserStatus />}></Route>
+          <Route path='/EmployeeLogin' element={<EmployeeLogin />}></Route>
+          <Route path='/EmployeeDetails' element={<EmployeeDetails />}></Route>
+          <Route path='/AdminLogin' element={<AdminLogin />}></Route>
+          <Route path='/AdminPage' element={<AdminPage />}></Route>
+          <Route path='/' element={<FixMyRideHomepage />}></Route>
+          <Route path='/ForgotPass' element={<Forgotpass />}></Route>
+        </Routes>
+      </Router>
+    </div>
 
-   
-  <Router>
-    <Routes>
-       
-         <Route path='/Registerpage' element={<Registerpage/>}></Route>
-          <Route path='/LoginPage' element={<LoginPage/>}></Route>
-          <Route path='/UserCarDetails' element={<UserCarDetails/>}></Route>
-          <Route path='/UserStatus' element={<UserStatus/>}></Route>
-          <Route path='/EmployeeLogin' element={<EmployeeLogin/>}></Route>
-          <Route path='/EmployeeDetails' element={<EmployeeDetails/>}></Route>
-          <Route path='/AdminLogin' element={<AdminLogin/>}></Route>
-          <Route path='/AdminPage' element={<AdminPage/>}></Route>
-          <Route path='/' element={<FixMyRideHomepage/>}></Route>
-
-          <Route path='/ForgotPass' element={<Forgotpass/>}></Route>
-    </Routes>
-  </Router>
- </div>
-
- 
+  )
 }
 
 export default App;
