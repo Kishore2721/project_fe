4652628717f8b5368e79ad8ecc1f83a08f3a947f
kishore2721 <kishoreg2721@gmail.com>
2025-06-15 20:30:19 +0530

    merged a and v with d

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,34 @@
+import Registerpage from "./components/RegisterPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./components/LoginPage";
 
-import Registerpage from './components/RegisterPage';
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
-import LoginPage from './components/LoginPage';
-
-import FixMyRideHomepage from './components/FixMyRideHomepage';
-import Forgotpass from './components/Forgotpass';
+import FixMyRideHomepage from "./components/FixMyRideHomepage";
+import Forgotpass from "./components/Forgotpass";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
-
-import UserCarDetails from './components/UserCarDetails';
-import UserStatus from './components/UserStatus';
-import Employee from './components/Employee';
-import UpdateEmployee from './components/UpdateEmployee';
-
-
-
-
+import UserCarDetails from "./components/UserCarDetails";
+import UserStatus from "./components/UserStatus";
+import Employee from "./components/Employee";
+import UpdateEmployee from "./components/UpdateEmployee";
 
 function App() {
-
-  return(
+  return (
     <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<FixMyRideHomepage />}></Route>
+          <Route path="/Registerpage" element={<Registerpage />}></Route>
+          <Route path="/LoginPage" element={<LoginPage />}></Route>
 
-   
-  <Router>
-    <Routes>
-         <Route path='/' element={<FixMyRideHomepage/>}></Route>
-         <Route path='/Registerpage' element={<Registerpage/>}></Route>
-          <Route path='/LoginPage' element={<LoginPage/>}></Route>
-         
-             <Route path='/ForgotPass' element={<Forgotpass/>}></Route>
-       
-         
-          <Route path='/UserCarDetails' element={<UserCarDetails/>}></Route>
-          <Route path='/UserStatus' element={<UserStatus/>}></Route>
-        <Route path='/employee' element={<Employee/>}></Route>
-          <Route path='/UpdateEmployee' element={<UpdateEmployee/>}></Route>
-         
-    </Routes>
-  </Router>
- </div>
+          <Route path="/ForgotPass" element={<Forgotpass />}></Route>
 
-  )
+          <Route path="/UserCarDetails" element={<UserCarDetails />}></Route>
+          <Route path="/UserStatus" element={<UserStatus />}></Route>
+          <Route path="/employee" element={<Employee />}></Route>
+          <Route path="/UpdateEmployee" element={<UpdateEmployee />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
