0d635e93822cad14ea605cfa70ee052eb73935e9
kishore2721 <kishoreg2721@gmail.com>
2025-06-15 20:21:46 +0530

    merged 2 branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 
-<<<<<<< HEAD
 import Registerpage from './components/RegisterPage';
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
@@ -7,7 +6,6 @@ import LoginPage from './components/LoginPage';
 import FixMyRideHomepage from './components/FixMyRideHomepage';
 import Forgotpass from './components/Forgotpass';
 import "bootstrap/dist/css/bootstrap.min.css";
-=======
 
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 
@@ -15,7 +13,7 @@ import UserCarDetails from './components/UserCarDetails';
 import UserStatus from './components/UserStatus';
 import Employee from './components/Employee';
 import UpdateEmployee from './components/UpdateEmployee';
->>>>>>> 29450c74f640e4964da434ee2a66d8d0a116f907
+
 
 
 
@@ -28,21 +26,18 @@ function App() {
    
   <Router>
     <Routes>
-<<<<<<< HEAD
          <Route path='/' element={<FixMyRideHomepage/>}></Route>
          <Route path='/Registerpage' element={<Registerpage/>}></Route>
           <Route path='/LoginPage' element={<LoginPage/>}></Route>
          
-<Route path='/ForgotPass' element={<Forgotpass/>}></Route>
-=======
+             <Route path='/ForgotPass' element={<Forgotpass/>}></Route>
        
          
           <Route path='/UserCarDetails' element={<UserCarDetails/>}></Route>
           <Route path='/UserStatus' element={<UserStatus/>}></Route>
-        <Route path='/' element={<Employee/>}></Route>
+        <Route path='/employee' element={<Employee/>}></Route>
           <Route path='/UpdateEmployee' element={<UpdateEmployee/>}></Route>
          
->>>>>>> 29450c74f640e4964da434ee2a66d8d0a116f907
     </Routes>
   </Router>
  </div>
